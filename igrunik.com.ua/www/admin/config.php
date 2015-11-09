fef597333fa0d3cdb3f9fae68a83d6ba75a93f6a
Admin <sepaev@gmail.com>
2015-11-09 14:07:19 +0200

    igrunok.com.ua

---
+++ igrunik.com.ua/www/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.igrunik.com.ua/admin/');
+define('HTTP_CATALOG', 'http://www.igrunik.com.ua/');
+// HTTPS
+define('HTTPS_SERVER', 'http://www.igrunik.com.ua/admin/');
+define('HTTPS_CATALOG', 'http://www.igrunik.com.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/igrunik/igrunik.com.ua/www/admin/');
+define('DIR_SYSTEM', '/home/igrunik/igrunik.com.ua/www/system/');
+define('DIR_DATABASE', '/home/igrunik/igrunik.com.ua/www/system/database/');
+define('DIR_LANGUAGE', '/home/igrunik/igrunik.com.ua/www/admin/language/');
+define('DIR_TEMPLATE', '/home/igrunik/igrunik.com.ua/www/admin/view/template/');
+define('DIR_CONFIG', '/home/igrunik/igrunik.com.ua/www/system/config/');
+define('DIR_IMAGE', '/home/igrunik/igrunik.com.ua/www/image/');
+define('DIR_CACHE', '/home/igrunik/igrunik.com.ua/www/system/cache/');
+define('DIR_DOWNLOAD', '/home/igrunik/igrunik.com.ua/www/download/');
+define('DIR_LOGS', '/home/igrunik/igrunik.com.ua/www/system/logs/');
+define('DIR_CATALOG', '/home/igrunik/igrunik.com.ua/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'igrunik.mysql.ukraine.com.ua');
+define('DB_USERNAME', 'igrunik_db');
+define('DB_PASSWORD', 'BzDfCDLy');
+define('DB_DATABASE', 'igrunik_db');
+define('DB_PREFIX', '');
+
+?>
